c7d3e50ec585cdbf3c5a9f2a15f9a1561c918e7b
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-02-19 12:52:30 -0300

    Creating test for show perfil user of usecase

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,72 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { AuthenticateUserUseCase } from '../authenticateUser/AuthenticateUserUseCase';
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { ICreateUserDTO } from "../createUser/ICreateUserDTO";
+import { ShowUserProfileError } from "./ShowUserProfileError";
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let createUserUseCase: CreateUserUseCase;
+let showUserProfileUseCase: ShowUserProfileUseCase;
+
+describe("Authenticate User", () => {
+	beforeEach(() => {
+		inMemoryUsersRepository = new InMemoryUsersRepository();
+		createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+		authenticateUserUseCase = new AuthenticateUserUseCase(inMemoryUsersRepository);
+        showUserProfileUseCase = new ShowUserProfileUseCase(inMemoryUsersRepository);
+	});
+
+	it("should be able of to obtain the return of user ", async () => {
+		const newUser: ICreateUserDTO = {
+			name: "Dani",
+			email: "dani@gmail.com",
+			password: "1234"
+		}
+
+		await createUserUseCase.execute(newUser);
+
+		const result = await authenticateUserUseCase.execute({
+			email: newUser.email,
+			password: newUser.password
+		});
+
+		const { id } = result.user;
+
+		const user = await showUserProfileUseCase.execute(id as string);
+		
+		expect(user.id).toEqual(id);
+	});
+
+	it("should not be able of to obtain the return of other users ", async () => {
+
+		const newUser: ICreateUserDTO = {
+			name: "Dani",
+			email: "dani@gmail.com",
+			password: "1234"
+		};
+
+		await createUserUseCase.execute(newUser);
+
+		const otherUser = {
+			id: "7",
+			name: "Diego",
+			email: "diego@gmail.com",
+			password: "1234"
+		};
+	
+		const result = await authenticateUserUseCase.execute({
+			email: newUser.email,
+			password: newUser.password
+		});
+
+		// Try to return another user's profile in your profile
+		expect(async () => {
+			await showUserProfileUseCase.execute(result.user.id = otherUser.id);
+		}).rejects.toBeInstanceOf(ShowUserProfileError);
+	});
+
+	
+
+});
